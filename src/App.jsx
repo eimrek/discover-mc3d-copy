18af9847541680640163bc350e25902def8b8b67
Kristjan Eimre <kristjaneimre@gmail.com>
2024-11-13 22:11:40 +0200

    switch to browserrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 import "./App.css";
 
-import { Routes, Route, HashRouter } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 
 import MainPage from "./MainPage";
 import DetailPage from "./DetailPage";
 
 function App() {
   return (
-    <HashRouter>
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/details/:id/:method" element={<DetailPage />} />
-      </Routes>
-    </HashRouter>
+    <BrowserRouter basename={import.meta.env.VITE_BASE_PATH || "/"}>
+    <Routes>
+      <Route path="/" element={<MainPage />} />
+      <Route path="/details/:id/:method" element={<DetailPage />} />
+    </Routes>
+  </BrowserRouter>
   );
 }
 
